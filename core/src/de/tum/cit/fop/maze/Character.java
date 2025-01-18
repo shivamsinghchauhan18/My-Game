0652e7279b4001dfbc1f76cedce1f25c7263eb3a
shivamsinghchauhan18 <shivam.chauhan@tum.de>
2025-01-18 22:49:15 +0100

    Character Class: - Character Initialisation - Current Animation based on Direction - Execution of setAnimation

---
+++ core/src/de/tum/cit/fop/maze/Character.java
@@ -0,0 +1,77 @@
+package de.tum.cit.fop.maze;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public abstract class Character extends GameObject {
+
+    public enum Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
+
+    private Direction direction;
+    private Animation<TextureRegion> leftAnimation;
+    private Animation<TextureRegion> rightAnimation;
+    private Animation<TextureRegion> upAnimation;
+    private Animation<TextureRegion> downAnimation;
+
+    private float prevX;
+    private float prevY;
+
+    public Character(float x, float y, int rectWidth, int rectHeight) {
+        super(x, y, rectWidth, rectHeight);
+        this.prevX = x;
+        this.prevY = y;
+        this.direction = Direction.DOWN; // Default direction
+    }
+
+    public abstract void update(float delta);
+
+
+    public abstract void draw(SpriteBatch spriteBatch);
+
+    public Animation<TextureRegion> getCurrentAnimation() {
+        return switch (direction) {
+            case LEFT -> leftAnimation;
+            case RIGHT -> rightAnimation;
+            case UP -> upAnimation;
+            case DOWN -> downAnimation;
+        };
+    }
+
+    public void setDirection(Direction direction) {
+        if (direction == null) {
+            throw new IllegalArgumentException("Direction cannot be null");
+        }
+        this.direction = direction;
+    }
+
+    public void setPrevX(float prevX) {
+        this.prevX = prevX;
+    }
+
+    public void setPrevY(float prevY) {
+        this.prevY = prevY;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public float getPrevX() {
+        return prevX;
+    }
+
+    public float getPrevY() {
+        return prevY;
+    }
+
+    public void setAnimations(Animation<TextureRegion> leftAnimation, Animation<TextureRegion> rightAnimation,
+                              Animation<TextureRegion> upAnimation, Animation<TextureRegion> downAnimation) {
+        this.leftAnimation = leftAnimation;
+        this.rightAnimation = rightAnimation;
+        this.upAnimation = upAnimation;
+        this.downAnimation = downAnimation;
+    }
+}
